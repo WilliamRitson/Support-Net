170f2fec19a70cc69cfd8c27b528464841918c79
dchigullapally <dchigull@ucsc.edu>
2016-01-30 13:41:14 -0800

    normal email login fixed to save username and email, then redirect to account page

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -44,7 +44,7 @@ angular.module('hackucscApp')
       }
 
       function createProfile(user) {
-        var ref = Ref.child('users', user.uid), def = $q.defer();
+        var ref = Ref.child(/*'users', */'users/' + user.uid), def = $q.defer();
         ref.set({email: email, name: firstPartOfEmail(email)}, function(err) {
           $timeout(function() {
             if( err ) {
@@ -73,7 +73,7 @@ angular.module('hackucscApp')
   
 
     function redirect() {
-      $location.path('/home');
+      $location.path('/account');
     }
 
     function showError(err) {
