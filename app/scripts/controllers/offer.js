8f79ac7ed4b7e7ab2958f273c2a1c1241030dc48
William Ritson <william.ritson@gmail.com>
2016-01-30 23:58:55 -0800

    Fixed errors in build process.
    Fixed a bug where images were not compressed properly.
    
    Fixed a bug where images were renamed and thus broken when they come from
    code.
    
    Fixed an issue where self closing spans were broken in the minified
    version.

--- app/scripts/controllers/offer.js
+++ app/scripts/controllers/offer.js
@@ -10,16 +10,14 @@
 angular.module('hackucscApp')
   .controller('OfferCtrl', function ($scope, $routeParams, $firebaseObject, $firebaseArray, Ref, categories, $location) {
 
-      $scope.offer = $firebaseObject(Ref.child("offer").child($routeParams.id))
+      $scope.offer = $firebaseObject(Ref.child('offer').child($routeParams.id));
       $scope.offer.$loaded().then(function(offer){
-         console.log(offer);
-         $scope.matchingRequest = $firebaseObject(Ref.child("request").child(offer.request));
-         console.log(offer.requester);
-         $scope.org = $firebaseObject(Ref.child("organisation").child(offer.requester));
+         $scope.matchingRequest = $firebaseObject(Ref.child('request').child(offer.request));
+         $scope.org = $firebaseObject(Ref.child('organisation').child(offer.requester));
       });
 
       $scope.updateOffer = function(offer){
          offer.$save();
-         $location.url("/home");
+         $location.url('/home');
       };
   });
